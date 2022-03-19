d9f320e9059fc6efcb07e3430a37acc7a941d65a
Aadarsh K <aadarshkannan111@gmail.com>
2022-03-19 10:32:36 +0000

    commit

--- JAVA/Add2Num.java
+++ JAVA/Add2Num.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Add2Num {
     public static void main(String[] args) {
         int x,y,sum;
-        Scanner myobj = new Scanner(System.in);
-        System.out.println("Enter the two numbers");
-        x = myobj.nextInt();
-        y = myobj.nextInt();
+        try (Scanner myobj = new Scanner(System.in)) {
+            System.out.println("Enter the two numbers");
+            x = myobj.nextInt();
+            y = myobj.nextInt();
+        }
         sum = x+y;
         System.out.println("The sum is " + sum);
     }
---
+++ JAVA/Examples/FreqofChar.java
@@ -0,0 +1,12 @@
+// To find the frequency of characters
+
+package Examples;
+
+public class FreqofChar {
+    public static void main(String[] args) {
+         String str = "Talking to the Moon";
+         char String[] = str.toCharArray();
+         System.out.println(String);
+
+        }
+}
