2c0d419f9370d15b7f64b3f1c3419c81dbffc2fc
Aadarsh Kannan <aadarshkannan111@gmail.com>
2022-07-20 17:20:58 +0000

    Delete files

--- PYTHON/Mongodb.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-myclient = pymongo.MongoClient("mongodb://localhost:3000/")
-mydb = myclient["mydatabase"]
-mycol = mydb["Songs"]
-mydict = { "Name": "River", "Artist": "Eminem" }
-
-x = mycol.insert_one(mydict)
-
-print(mydb.list_collection_names())
